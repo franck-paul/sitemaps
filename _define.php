468f36057f3d611464695c93cf2224e4077aff3a
franck <carnet.franck.paul@gmail.com>
2023-06-27 09:46:19 +0200

    Release 3.1.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Sitemaps',
     'Add XML Sitemaps',
     'Pep and contributors',
-    '3.1.1',
+    '3.1.2',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
