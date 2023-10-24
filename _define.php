85f1601998a2e9d3bcd4351b20be87750daa68e9
franck <carnet.franck.paul@gmail.com>
2023-10-25 00:20:06 +0200

    New release 5.1

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Sitemaps',
     'Add XML Sitemaps',
     'Pep and contributors',
-    '5.0',
+    '5.1',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
