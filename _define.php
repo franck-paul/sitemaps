d7054dc284e64aaf3b51cd3cd5da7be07ec723da
franck <carnet.franck.paul@gmail.com>
2025-09-09 21:02:51 +0200

    New release 9.2

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Sitemaps',
     'Add XML Sitemaps',
     'Pep and contributors',
-    '9.1',
+    '9.2',
     [
-        'date'     => '2025-09-07T15:48:46+0200',
+        'date'     => '2025-09-09T21:02:51+0200',
         'requires' => [
             ['core', '2.36'],
             ['TemplateHelper'],
